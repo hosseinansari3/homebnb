51b51f23a710767dd4b18b9b2fab2fc3917105d7
Hossein Ansari <hosseinansari3@gmail.com>
2023-09-20 14:17:57 +0430

    do some changes to listing and user models

--- models/Listing.ts
+++ models/Listing.ts
@@ -1,18 +1,21 @@
 import mongoose, { Schema } from "mongoose";
 
-const listingSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  imageSrc: String,
-  createdAt: { type: Date, default: Date.now },
-  category: String,
-  roomCount: Number,
-  bathroomCount: Number,
-  guestCount: Number,
-  locationValue: String,
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  price: Number,
-});
+const listingSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    imageSrc: String,
+    createdAt: { type: Date, default: Date.now },
+    category: String,
+    roomCount: Number,
+    bathroomCount: Number,
+    guestCount: Number,
+    locationValue: String,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    price: Number,
+  },
+  { timestamps: true }
+);
 
 const Listing =
   mongoose.models.Listings || mongoose.model("Listings", listingSchema);
